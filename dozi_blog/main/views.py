f7d9d198d5e6bd1262e33c29d5bb16851bab7ccd
Chido100 <chido_dozie@hotmail.com>
2024-01-31 12:25:44 +0000

    Created views for main app

--- dozi_blog/main/views.py
+++ dozi_blog/main/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+
+# Display all posts
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'main/post/list.html', {'posts': posts})
+
+
+
+# Display single post
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'main/post/detail.html', {'post': post})
