c986c9bea8d5f09874c5cfe10f77af8c090f033b
Chido100 <chido_dozie@hotmail.com>
2024-02-01 12:54:09 +0000

    Created comments model

--- dozi_blog/main/models.py
+++ dozi_blog/main/models.py
@@ -34,9 +34,7 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-publish']
-        indexes = [
-            models.Index(fields=['-publish'])
-        ]
+        indexes = [models.Index(fields=['-publish'])]
 
 
     def __str__(self):
@@ -50,3 +48,22 @@ class Post(models.Model):
             self.publish.day,
             self.slug
         ])
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)   # allows for deactivation of inappropriate comments in the admin
+
+
+    class Meta:
+        ordering = ['created']
+        indexes = [models.Index(fields=['created']),]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
